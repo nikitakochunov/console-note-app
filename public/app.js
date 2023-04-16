92dd568fd154aeefe8905839c2422d96f868ab69
Nikita <tonno651@gmail.com>
2023-04-17 00:31:59 +0300

    edit

--- public/app.js
+++ public/app.js
@@ -4,10 +4,36 @@ document.addEventListener('click', async (event) => {
     await remove(id)
     event.target.closest('li').remove()
   }
+
+  if (event.target.dataset.type === 'edit') {
+    const id = event.target.dataset.id
+
+    const titleElement = event.target.closest('li').firstElementChild
+    const currentTitle = titleElement.innerText
+
+    const res = prompt('Enter a new title', currentTitle)
+
+    if (res === currentTitle || res === null) {
+      return
+    } else {
+      await edit(id, res)
+      titleElement.innerText = res
+    }
+  }
 })
 
 async function remove(id) {
   await fetch(`/${id}`, {
     method: 'DELETE',
+  })
+}
+
+async function edit(id, data) {
+  await fetch(`/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title: data }),
   })
 }
